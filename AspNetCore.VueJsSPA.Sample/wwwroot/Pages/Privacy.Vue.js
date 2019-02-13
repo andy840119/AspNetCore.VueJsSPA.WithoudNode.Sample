6127f78a563b812b06990523d298e67dc623d388
為什麼 <andy840119@gmail.com>
2019-02-13 11:57:00 +0800

    加上Home 和 Privacy 的 Page component

---
+++ AspNetCore.VueJsSPA.Sample/wwwroot/Pages/Privacy.Vue.js
@@ -0,0 +1,9 @@
+﻿var spaPrivacy = Vue.component("Privacy", {
+    template: `
+    <p>Use this page to detail your site's privacy policy.</p>
+  `,
+    props: ["title"],
+    $_veeValidate: {
+        validator: "new"
+    }
+});
